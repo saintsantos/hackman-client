35a6a60c74bd8e2febd14e2a7c6456a687f6c5b8
saintsantos <saintsantos1341@gmail.com>
2016-09-17 17:18:03 -0400

    fixed errors, migrated model to angular

--- app/components/teams/teams.controller.js
+++ app/components/teams/teams.controller.js
@@ -9,4 +9,30 @@ angular
             //Actually define what the function is supposed to do here
             console.log("This is a function triggered in a controller");
         }
+
+        $scope.underConstruction = function() {
+            document.getElementById("ConstMessage").innerHTML = "These buttons are under construction. Please check back once databases are up and running!";
+        }
+
+        $scope.loadTeamInfo = function(group) {
+            document.getElementById("groupName").innerHTML = group;
+            document.getElementById("InfoPanel").innerHTML = "This is the information for the team";
+        }
+
+        $scope.loadAnnouncementInfo = function(group, announce) {
+            document.getElementById("groupName").innerHTML = group;
+            document.getElementById("InfoPanel").innerHTML = announce;
+        }
+
+        $scope.loadMembers = function(group, num1, num2, num3) {
+            document.getElementById("groupName").innerHTML = group;
+            document.getElementById("InfoPanel").innerHTML = "DummyMember"+num1+"\t DummyMember"+num2+"\t DummyMember"+num3;
+        }
+
+        $scope.teamInfoPopUp = function() {
+            var team=prompt("Enter your team name:","teamName");
+            if(team!=null && team!="teamName"){
+            alert("We were unable to create the team:\n\t"+team+"\n as our databases are not yet up and running.\n Thank you for your understanding");
+            }
+        }
     });
