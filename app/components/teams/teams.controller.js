55aebdd1f8b6d9628242635450df4fed0b94c7c2
Steven Siwinski <StevesAir@dhcp035-164-110.wireless.buffalo.edu>
2016-10-01 17:05:33 -0400

    collapsable menus now working

--- app/components/teams/teams.controller.js
+++ app/components/teams/teams.controller.js
@@ -1,10 +1,17 @@
 angular
     //This is used for dependency calls. See teams.module.js to see the dependencies in action.
-    .module('teams.controller', ['ui.bootstrap'])
+    .module('teams.controller', [])
     //The exported name of the controller module, this is called in other modules and in html
     .controller('TeamsController', function($scope, $state, TeamService) {
         //An example of a controller for an html file.
         //In angular, in order to make a function callable, use $scope to make the function as shown below.
+        $scope.groups = [{
+          "X":"Magic Cam"},{
+          "X":"Super Freezer 5000"},{
+          "X":"Random Group"},{
+          "X":"HoloGen"
+        }]
+
         $scope.test = function() {
             //Actually define what the function is supposed to do here
             console.log("This is a function triggered in a controller");
@@ -35,12 +42,4 @@ angular
             alert("We were unable to create the team:\n\t"+team+"\n as our databases are not yet up and running.\n Thank you for your understanding");
             }
         }
-
-        $scope.sayHi = function() {
-            console.log("Http request");
-            TeamService.httpCall().then(function(hi) {
-                console.log("request %0",hi.data.hi);
-            });
-
-        }
-    });
+    })
