7bb312f9b176daff8e9de6fec7f636a383e48bfe
Olivia  Alphonce <Mine@dhcp020-211-201.wireless.buffalo.edu>
2016-10-29 12:48:32 -0400

    beginnings of a dummy prizes page. stil need to declare it everywhere, and give it a button so it can be accessed and put in the images

---
+++ app/components/prizes/prize.module.js
@@ -0,0 +1,7 @@
+//Use the module to connect everything together.
+
+angular
+    //All those modules are called here to glue them together
+    .module('prize.module', [
+        'prizes.controller'
+    ])
