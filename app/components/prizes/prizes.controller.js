ff82fbaf3f2429a3a52823e144322b202f27d5e7
saintsantos <saintsantos1341@gmail.com>
2016-11-20 17:04:35 -0500

    tweaked prizes call

--- app/components/prizes/prizes.controller.js
+++ app/components/prizes/prizes.controller.js
@@ -57,12 +57,12 @@ angular
 ];*/
 
   $scope.savePrize = function(prize, id) {
-      //PrizeService.editPrize(prize);
+      PrizeService.editPrize(prize);
       console.log(prize);
   }
 
   $scope.deletePrize = function(prize, id) {
-      //PrizeService.deletePrize(prize._id);
+      PrizeService.deletePrize(prize._id);
       $scope.prizes.splice(id, 1);
       console.log(prize);
 
