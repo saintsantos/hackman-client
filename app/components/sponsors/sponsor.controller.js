5bcd80ddc5daab884dd4fbea20d19cc114041471
Steven Siwinski <steve@dhcp035-163-021.wireless.buffalo.edu>
2016-11-20 15:33:19 -0500

    cleaned up some really nasty indents in the code... ED!!

--- app/components/sponsors/sponsor.controller.js
+++ app/components/sponsors/sponsor.controller.js
@@ -9,34 +9,78 @@ angular
         };
 
         $scope.title='Sponsors';
-        $scope.sponsorX=[
-            {
-                name:'Fanciful Sponsor',
-                about:"Here at Fanciful Sponsor, we believe that everything is fanciful, and strive to spread that mentality to the rest of the world. Fanciful walks in the park, fanciful flowers, fanciful poops. Fanciful everything.",
-                image:"../../assets/img/fancySponsor.JPG",
-                link:"https://placekitten.com/"
-            },
-            {
-                name:"Tables",
-                about:"Tables. Tables everywhere. All the tables. So many Tables.",
-                image: "../../assets/img/table.JPG",
-                link:"https://www.amazon.com/Melissa-Doug-Wooden-Table-Chairs/dp/B00CE69IGA/ref=sr_1_11?ie=UTF8&qid=1476554374&sr=8-11&keywords=table"
+
+        if ($window.localStorage.getItem('role') === 'admin') {
+            $scope.role = true;
+        } else {
+            $scope.role = false;
+        }
+
+        PrizeService.getPrizes().then(function(result) {
+            $scope.prizes = result.data;
+        })
+
+        $scope.manage = true;
+
+        console.log($scope.role);
+
+        $scope.savePrize = function(prize, id) {
+            //PrizeService.editPrize(prize);
+            console.log(prize);
+        }
+
+        $scope.deletePrize = function(prize, id) {
+            //PrizeService.deletePrize(prize._id);
+            $scope.prizes.splice(id, 1);
+            console.log(prize);
+
+        }
+
+        $scope.setPrize = function(prize, index) {
+            $scope.chozen_index = index;
+            $scope.chosen_prize = prize;
+            console.log(prize);
+        }
+
+        $scope.createPrize = function() {
+            prize = {
+              	name:'New Prize',
+                desc:'This is a new prize',
+                sponsor:'Yes there is'
             }
-    ];
-    $scope.sponsors = [
-  	{
-    	name:'Fanciful Sponsor',
-        about:'Here at Fanciful Sponsor, we believe that everything is fanciful, and strive to spread that mentality to the rest of the world. Fanciful walks in the park, fanciful flowers, fanciful poops. Fanciful everything.',
-        image:'../../assets/img/fancySponsor.JPG',
-        link:'https://placekitten.com/'
-  	},
-  	{
-    	name:'Tables',
-        about:'Tables. Tables everywhere. All the tables. So many Tables.',
-        image:'../../assets/img/table.JPG',
-        link:'https://www.amazon.com/Melissa-Doug-Wooden-Table-Chairs/dp/B00CE69IGA/ref=sr_1_11?ie=UTF8&qid=1476554374&sr=8-11&keywords=table'
-  	}
-  ];
+            PrizeService.newPrize(prize);
+            $scope.prizes.push(prize);
+        }
+
+        
+        //         $scope.sponsorX=[
+        //             {
+        //                 name:'Fanciful Sponsor',
+        //                 about:"Here at Fanciful Sponsor, we believe that everything is fanciful, and strive to spread that mentality to the rest of the world. Fanciful walks in the park, fanciful flowers, fanciful poops. Fanciful everything.",
+        //                 image:"../../assets/img/fancySponsor.JPG",
+        //                 link:"https://placekitten.com/"
+        //             },
+        //             {
+        //                 name:"Tables",
+        //                 about:"Tables. Tables everywhere. All the tables. So many Tables.",
+        //                 image: "../../assets/img/table.JPG",
+        //                 link:"https://www.amazon.com/Melissa-Doug-Wooden-Table-Chairs/dp/B00CE69IGA/ref=sr_1_11?ie=UTF8&qid=1476554374&sr=8-11&keywords=table"
+        //             }
+        //     ];
+        //     $scope.sponsors = [
+        //   	{
+        //     	name:'Fanciful Sponsor',
+        //         about:'Here at Fanciful Sponsor, we believe that everything is fanciful, and strive to spread that mentality to the rest of the world. Fanciful walks in the park, fanciful flowers, fanciful poops. Fanciful everything.',
+        //         image:'../../assets/img/fancySponsor.JPG',
+        //         link:'https://placekitten.com/'
+        //   	},
+        //   	{
+        //     	name:'Tables',
+        //         about:'Tables. Tables everywhere. All the tables. So many Tables.',
+        //         image:'../../assets/img/table.JPG',
+        //         link:'https://www.amazon.com/Melissa-Doug-Wooden-Table-Chairs/dp/B00CE69IGA/ref=sr_1_11?ie=UTF8&qid=1476554374&sr=8-11&keywords=table'
+        //   	}
+        //   ];
 
 
 });
