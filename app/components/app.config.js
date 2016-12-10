cef18089dbde0cb2c6791619b349277ba5273555
Alex <thexanderproject@gmail.com>
2016-12-10 17:12:21 -0500

    fixed layout of main page, added toggle button

--- app/components/app.config.js
+++ app/components/app.config.js
@@ -3,7 +3,7 @@ angular
     .module('app.config', ['ui.router', 'auth0.lock', 'auth.service'])
 
     .constant('urlConstant', {
-        baseUrl: 'http://localhost:3000'
+        baseUrl: '//54.244.170.44'
     })
 
     .config(function($stateProvider, $urlRouterProvider) {
